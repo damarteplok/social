602e9c58e29303d763c5a64c73c93c0ad0404afe
damar <damarhuda@live.com>
2024-10-21 08:10:07 +0700

    users

--- cmd/migrate/migrations/000005_add_comments.up.sql
+++ cmd/migrate/migrations/000005_add_comments.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS comments (
     id bigserial PRIMARY KEY,
-    post_id bigserial NOT NULL,
-    user_id bigserial NOT NULL,
+    post_id BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
     content TEXT NOT NULL,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
 );
